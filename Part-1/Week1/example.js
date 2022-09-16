dd4aab83e30b294debaa5d6b4c10288945f5ac50
Khalil Olaes <rolaes@my.bcit.ca>
2022-09-16 12:15:28 -0700

    adding moe code

--- Part-1/Week1/example.js
+++ Part-1/Week1/example.js
@@ -3,3 +3,32 @@
 //Declare a variable car
 
 var car;
+
+//camelCase
+//varialbe calledbugs collected and initiate it to value of 4
+var ugsCollected =4;
+
+//PascalCase
+//variable case called bugs collected and initialise at the value of 4
+var Bugscollected = 4;
+
+//SnakeCase
+// varialbe called bugs collected and initialise it to the value of 4
+var Bugs_collected = 4; 
+
+//initialise the variable electronics box to the word cpu/
+//use camelCase
+
+var electrinicBox= "cpu";
+
+// Boolean
+//Initilise the variable called proceed to the value of true
+
+var proceed  = true;
+var i = 0;
+
+
+while (proceed)  {
+    i++;
+    proceed=false
+}
---
+++ Part-1/Week1/test.js
@@ -0,0 +1,34 @@
+// Boolean
+//Initilise the variable called proceed to the value of true
+
+var proceed  = true;
+var i = 0;
+
+
+while (proceed)  {
+    i++; // i = 1;
+    proceed=false
+}
+
+console.log(i);
+
+//data Type - object
+/**
+ * 6-4-1 Create a variable called grannySmith and add the 
+ * following values (use camelCase)
+ * colour of the apple is green 
+ * oigin location is australia
+ * origin code is 1968 
+ * Then console log out the color from
+ * the object
+ */
+
+var grannySmith = {
+    color:"green", 
+    originlocation: "australia",
+    originDate: 1868
+
+
+};
+
+console.log(grannySmith);
